d3b35caaf9b7571cdec2b5fc8df2bee8d95bf881
BlueBeret <63086194+BlueBeret@users.noreply.github.com>
2022-04-21 11:41:44 +0700

    add new service 6 who provide data

---
+++ dataserver/dataserver.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask import jsonify
+import json
+
+app = Flask(__name__)
+
+with open('./data.json', 'r') as productList:
+   data = json.load(productList)
+
+@app.route("/products")
+def lists():
+    
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5006)
